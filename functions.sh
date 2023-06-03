2427f8e4d89225ad7d97c3572f4f86ab9aef57ee
Gabriel Guerra <gfranklings@gmail.com>
2023-06-03 14:25:47 -0400

    [chore] better organisation for building environment

--- functions.sh
+++ functions.sh
@@ -1,11 +1,13 @@
+TEMP=$(mktemp -p)
+
 into_tar_source () {
     tar -xvf ${@}.tar*
     cd $@
 }
 
 cleanup () {
-    cd ../
-    rm -rvf ./${@}*
+    cd /
+    rm -rvf /tmp/$TEMP
 }
 
 # load the builder
