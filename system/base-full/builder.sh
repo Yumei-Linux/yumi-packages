1c6a8aa637508dbce605c217d978492a507f1257
Gabriel Guerra <gfranklings@gmail.com>
2023-06-03 23:13:26 -0400

    [feat] starting to make the system/base-full pkg

---
+++ system/base-full/builder.sh
@@ -0,0 +1,22 @@
+ROOT=/var/yumi/system/base-full
+
+source () {
+    cd /sources
+    test -d ${@} && rm -rvf ${@}
+    tar -xvf ./${@}.tar* || exit 1
+    cd ${@}
+}
+
+load () {
+    . $ROOT/pkgs/${@}
+}
+
+packages=(
+    man-pages.sh
+)
+
+for pkg in ${packages[@]}; do
+    echo "** COMPILING ${pkg} [BASE FULL]..."
+    sleep 1
+    load $pkg
+done
---
+++ system/base-full/pkgs/man-pages.sh
@@ -0,0 +1,2 @@
+source man-pages-6.03
+make prefix=/usr install
