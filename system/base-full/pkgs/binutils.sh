cb82bf351a7c8bee591c03f2829d5878db4a3dcb
Gabriel Guerra <gfranklings@gmail.com>
2023-06-04 00:16:13 -0400

    [chore] packaged binutils

--- system/base-full/pkgs/binutils.sh
+++ system/base-full/pkgs/binutils.sh
@@ -1,3 +1,19 @@
 source binutils-2.40
 
-expect -c "spawn ls"
+mkdir -pv build ; cd build
+
+../configure --prefix=/usr \
+    --sysconfdir=/etc \
+    --enable-gold \
+    --enable-ld=default \
+    --enable-plugins \
+    --enable-shared \
+    --disable-werror \
+    --enable-64-bit-bfd \
+    --with-system-zlib
+
+make tooldir=/usr
+make tooldir=/usr install
+
+rm -fv /usr/lib/lib{bfd,ctf,ctf-nobfd,sframe,opcodes}.a
+rm -fv /usr/share/man/man1/{gprofng,gp-*}.1
