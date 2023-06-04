c735ce41a19bfb6032ec0937cf0247f92411a0b1
Gabriel Guerra <gfranklings@gmail.com>
2023-06-04 18:44:34 -0400

    [chore] some testing

--- security/ca-certificates/builder.sh
+++ security/ca-certificates/builder.sh
@@ -1,5 +1,22 @@
-pwd
+whydidyouremovesources () {
+    echo "[Fatal] cannot find ca-certificates source files... did you remove /sources?"
+    exit 1
+}
 
-ls
+CERTDATA=/sources/ca-certificates/certdata.txt
+MAKECA=/sources/ca-certificates/make-ca.sh-20170514
+
+if [ ! -f "$CERTDATA" ]; then
+    whydidyouremovesources
+fi
+
+if [ -f "$MAKECA" ]; then
+    whydidyouremovesources
+fi
+
+cp -rvf "$CERTDATA" "$(dirname $0)"
+cp -rvf "$MAKECA" "$(dirname $0)"
+
+pwd ; ls
 
 cleanup
