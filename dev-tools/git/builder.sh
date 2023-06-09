47f6899670574c86f7584ce83fc7eec2a1c19e32
Gabriel Guerra <gfranklings@gmail.com>
2023-06-09 15:30:14 -0400

    [chore] git: stopping compiling man manual

--- dev-tools/git/builder.sh
+++ dev-tools/git/builder.sh
@@ -4,7 +4,6 @@ into_tar_source git-2.41.0
     --with-gitconfig=/etc/gitconfig \
     --with-python=python3
 
-_make && _make html && _make man
+_make && _make html
 _make perllibdir=/usr/lib/perl5/5.36/site_perl install
-_make install-man
 _make htmldir=/usr/share/doc/git-2.41.0 install-html
