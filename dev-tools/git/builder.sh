1e86f6f06f7c793f6b7fb4b16a507de7e5d95c29
Gabriel Guerra <gfranklings@gmail.com>
2023-06-09 15:34:05 -0400

    [refactor] git: stop compiling manuals, using precompiled documentation

--- dev-tools/git/builder.sh
+++ dev-tools/git/builder.sh
@@ -4,6 +4,14 @@ into_tar_source git-2.41.0
     --with-gitconfig=/etc/gitconfig \
     --with-python=python3
 
-_make && _make html
-_make perllibdir=/usr/lib/perl5/5.36/site_perl install
-_make htmldir=/usr/share/doc/git-2.41.0 install-html
+_make && _make perllibdir=/usr/lib/perl5/5.36/site_perl install
+
+tar -xf ../git-manpages-2.41.0.tar.xz \
+    -C /usr/share/man --no-same-owner --no-overwrite-dir
+
+mkdir -vp   /usr/share/doc/git-2.41.0 &&
+tar   -xf   ../git-htmldocs-2.41.0.tar.xz \
+      -C    /usr/share/doc/git-2.41.0 --no-same-owner --no-overwrite-dir &&
+
+find        /usr/share/doc/git-2.41.0 -type d -exec chmod 755 {} \; &&
+find        /usr/share/doc/git-2.41.0 -type f -exec chmod 644 {} \;
