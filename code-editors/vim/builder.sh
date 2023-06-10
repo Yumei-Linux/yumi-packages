4cad1ab06d709afcc26f7932a1b4c9d3f0078c1f
Gabriel Guerra <gfranklings@gmail.com>
2023-06-10 09:10:06 -0400

    [feat] packaging vim

---
+++ code-editors/vim/builder.sh
@@ -0,0 +1,35 @@
+into_tar_source vim-9.0.1503
+
+echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
+echo '#define SYS_GVIMRC_FILE "/etc/gvimrc"' >> src/feature.h
+
+echo "[Q] Do you want --enable-gui=gtk3? [y/N] "
+read wantsgtk
+
+while [[ "$wantsgtk" != 'y' || "$wantsgtk" != 'n' || "$wantsgtk" != "" ]]; then
+    echo "  * Invalid answer, put y or n"
+    read wantsgtk
+fi
+
+if [[ "$wantsgtk" == "y" ]]; then
+    ./configure --prefix=/usr \
+        --with-features=huge \
+        --enable-gui=gtk3 \
+        --with-tlib=ncursesw
+else
+    ./configure --prefix=/usr \
+        --with-features=huge \
+        --with-tlib=ncursesw
+fi
+
+_make && _make install
+
+ln -snfv ../vim/vim90/doc /usr/share/doc/vim-9.0.1503
+
+rsync -avzcP --exclude="/dos/" --exclude="/spell/" \
+    ftp.nluug.nl::Vim/runtime/ ./runtime/
+
+make -C src installruntime &&
+vim -c ":helptags /usr/share/doc/vim-9.0.1503" -c ":q"
+
+echo "[I] Config files are /etc/vimrc and /etc/gvimrc, /etc/vimrc should already be filled by yumi's system/base-full pkg"
