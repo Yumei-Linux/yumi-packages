6218137036a0b730dbe580bd5104c672245a32f0
Gabriel Guerra <gfranklings@gmail.com>
2023-06-10 09:17:48 -0400

    [refactor] vim: stop compiling gtk3 thingies

--- code-editors/vim/builder.sh
+++ code-editors/vim/builder.sh
@@ -3,30 +3,9 @@ into_tar_source vim-9.0.1503
 echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
 echo '#define SYS_GVIMRC_FILE "/etc/gvimrc"' >> src/feature.h
 
-echo "[Q] Do you want --enable-gui=gtk3? [y/N] "
-read wantsgtk
-
-if [[ "$wantsgtk" != 'y' && "$wantsgtk" != 'n' && "$wantsgtk" != "" ]]; then
-    echo "* Invalid answer, put y or n. Retry building."
-    exit 1
-fi
-
-echo "wantsgtk -> $wantsgtk"
-
-if [[ "$wantsgtk" == "y" ]]; then
-    echo "[I] Configuring with --enable-gui=gtk3"
-    exit 1
-    ./configure --prefix=/usr \
-        --with-features=huge \
-        --enable-gui=gtk3 \
-        --with-tlib=ncursesw
-else
-    echo "bad"
-    exit 1
-    ./configure --prefix=/usr \
-        --with-features=huge \
-        --with-tlib=ncursesw
-fi
+./configure --prefix=/usr \
+    --with-features=huge \
+    --with-tlib=ncursesw
 
 _make && _make install
 
@@ -34,8 +13,5 @@ ln -snfv ../vim/vim90/doc /usr/share/doc/vim-9.0.1503
 
 rsync -avzcP --exclude="/dos/" --exclude="/spell/" \
     ftp.nluug.nl::Vim/runtime/ ./runtime/
-
-make -C src installruntime &&
-vim -c ":helptags /usr/share/doc/vim-9.0.1503" -c ":q"
 
 echo "[I] Config files are /etc/vimrc and /etc/gvimrc, /etc/vimrc should already be filled by yumi's system/base-full pkg"
